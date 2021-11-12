dc23ee2fb27af7132defdbcfebbf90d5015ff3a8
johnjc153 <153johnjc@gmail.com>
2021-11-11 18:47:48 -0700

    js alert

--- ongoing/disHealing/assets/js/main.js
+++ ongoing/disHealing/assets/js/main.js
@@ -209,7 +209,12 @@
   });
 
 
+  const btnClear = document.getElementById('submitbtn');
+  const inputs = document.querySelectorAll('input');
 
+  btnClear.addEventListener('click', () => {
+    inputs.forEach(input => input.vlaue = '');
+  });
 
   // document.getElementById("form").submit(function (e) {
   //   e.preventDefault();
